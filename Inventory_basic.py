b39cc8ae3f4ab794594b75e7618b099c635d58ca
steve <steve@steves-MacBook-Pro.local>
2022-04-05 16:39:04 -0600

    first commit

---
+++ Inventory_basic.py
@@ -0,0 +1,20 @@
+"""
+Inventory host values to stdout
+Stack overflow: https://stackoverflow.com/questions/3103178/how-to-get-the-system-info-with-python
+
+"""
+import platform
+import socket
+import re
+import uuid
+import psutil # not part of std lib
+
+print('platform:',platform.system())
+print('platform-release:',platform.release())
+print('architecture:',platform.machine())
+print('hostname:',socket.gethostname())
+print('ip-address:',socket.gethostbyname(socket.gethostname()))
+print('mac-address:',':'.join(re.findall('..', '%012x' % uuid.getnode())))
+print('ram:',str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB")
+print('cpu %:',psutil.cpu_percent(1))
+print('cpu cores:',psutil.cpu_count())
